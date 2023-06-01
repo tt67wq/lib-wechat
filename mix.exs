a039ea6e89a2c120f160230a2d27d1106e934ea3
tt67wq <tt67wq@aliyun.com>
2023-06-01 22:58:58 +0800

    feat: set project

--- mix.exs
+++ mix.exs
@@ -1,13 +1,21 @@
 defmodule LibWechat.MixProject do
   use Mix.Project
 
+  @name "lib_wechat"
+  @version "0.1.0"
+  @repo_url "https://github.com/tt67wq/lib-wechat"
+  @description "A library for WeChat API"
+
   def project do
     [
       app: :lib_wechat,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      source_url: @repo_url,
+      name: @name,
+      package: package()
     ]
   end
 
@@ -23,9 +31,19 @@ defmodule LibWechat.MixProject do
     [
       {:nimble_options, "~> 0.5"},
       {:jason, "~> 1.4"},
-      {:finch, "~> 0.16"}
+      {:finch, "~> 0.16"},
+      {:ex_doc, "~> 0.29", only: :dev, runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp package do
+    [
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => @repo_url
+      }
     ]
   end
 end
